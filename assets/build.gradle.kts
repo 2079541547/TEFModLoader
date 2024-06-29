52d32b024afb1bb0105319e2fdf59645ec72a187
EternalFuture゙ <142726636+2079541547@users.noreply.github.com>
2024-06-29 20:28:22 +0800

    更新文件

--- assets/build.gradle.kts
+++ assets/build.gradle.kts
@@ -49,14 +49,6 @@ android {
 
 dependencies{
     implementation(fileTree("libs"))
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
