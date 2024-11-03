ff19f4c16b0d1001f831169b21ccb3419d57c105
EternalFuture゙ <142726636+2079541547@users.noreply.github.com>
2024-11-03 11:33:18 +0800

    更新文件

--- app/src/main/java/silkways/terraria/efmodloader/ui/preference/ThemePreference.kt
+++ app/src/main/java/silkways/terraria/efmodloader/ui/preference/ThemePreference.kt
@@ -5,7 +5,6 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.PopupMenu
 import androidx.preference.Preference
