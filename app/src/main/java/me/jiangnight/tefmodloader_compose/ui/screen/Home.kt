bb5e338884ab68ef9495d0877235b387b62205bd
JiangNight <wan1298178043@gmail.com>
2024-11-04 22:45:58 +0800

    主页完毕，方案二

--- app/src/main/java/me/jiangnight/tefmodloader_compose/ui/screen/Home.kt
+++ app/src/main/java/me/jiangnight/tefmodloader_compose/ui/screen/Home.kt
@@ -7,12 +7,13 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -26,6 +27,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -38,165 +40,175 @@ import com.ramcosta.composedestinations.annotation.RootNavGraph
 import me.jiangnight.tefmodloader_compose.R
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @RootNavGraph(start = true)
 @Destination
 @Composable
 fun HomeScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
+    Scaffold(
+        topBar = {
+            CustomTopBar("首页")
+        },
+        content = { innerPadding ->
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(0.dp) // 确保内容不会被顶部栏遮挡
+                    .padding(horizontal = 10.dp),
+                contentPadding = innerPadding,
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                // 卡片1：欢迎信息
+                item {
+                    Card(
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.secondaryContainer
+                        ),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 1.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp)
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.greetings_1),
+                                fontSize = 15.sp,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Text(
+                                text = "YiYan",
+                                modifier = Modifier
+                                    .padding(start = 10.dp, end = 5.dp)
+                                    .fillMaxWidth()
+                            )
+                            Text(
+                                text = stringResource(id = R.string.yiyan_text),
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }
+                }
+                item {
+                    SpacerAndButtonsSection()
+                }
+                // 更新日志的卡片
+                item {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant
+                        ),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 1.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp)
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.logs),
+                                fontSize = 15.sp,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Text(
+                                text = stringResource(id = R.string.logs_title_150),
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Text(
+                                text = stringResource(id = R.string.logs_text_150),
+                                fontSize = 13.sp,
+                                modifier = Modifier
+                                    .padding(start = 10.dp)
+                                    .fillMaxWidth()
+                            )
+                        }
+                    }
+                }
+
+                item {
+
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun SpacerAndButtonsSection() {
+
+    Spacer(modifier = Modifier.height(10.dp))
+
+    // 第一行按钮
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        TopBar()
-
-        // LazyColumn 用于创建可滚动的内容列表
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 10.dp)
+        Button(
+            onClick = { /* TODO: About action */ },
+            modifier = Modifier.weight(1f),
+            contentPadding = PaddingValues(start = 10.dp)
         ) {
-            item {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 10.dp),
-                    colors = CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.secondaryContainer
-                    ),
-                    elevation = CardDefaults.cardElevation(defaultElevation = 1.dp)
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(10.dp)
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.greetings_1),
-                            fontSize = 15.sp,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Text(
-                            text = "YiYan",
-                            modifier = Modifier
-                                .padding(start = 10.dp, end = 5.dp)
-                                .fillMaxWidth()
-                        )
-                        Text(
-                            text = stringResource(id = R.string.yiyan_text),
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                    }
-                }
+            Icon(Icons.Default.People, contentDescription = "About Icon")
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = stringResource(id = R.string.about))
+        }
 
-                Spacer(modifier = Modifier.height(10.dp))
-            }
+        Button(
+            onClick = { /* TODO: Settings action */ },
+            modifier = Modifier.weight(1f),
+            contentPadding = PaddingValues(start = 10.dp)
+        ) {
+            Icon(Icons.Default.Settings, contentDescription = "Settings Icon")
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = stringResource(id = R.string.settings))
+        }
+    }
 
-            item {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(10.dp)
-                ) {
-                    Button(
-                        onClick = { /* TODO: About action */ },
-                        modifier = Modifier.weight(1f),
-                        contentPadding = PaddingValues(start = 10.dp)
-                    ) {
-                        Icon(Icons.Default.People, contentDescription = "About Icon")
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Text(text = stringResource(id = R.string.about))
-                    }
+    Spacer(modifier = Modifier.height(10.dp))
 
-                    Button(
-                        onClick = { /* TODO: Settings action */ },
-                        modifier = Modifier.weight(1f),
-                        contentPadding = PaddingValues(start = 10.dp)
-                    ) {
-                        Icon(Icons.Default.Settings, contentDescription = "Settings Icon")
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Text(text = stringResource(id = R.string.settings))
-                    }
-                }
+    // 第二行按钮
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Button(
+            onClick = { /* TODO: Feedback action */ },
+            modifier = Modifier.weight(1f),
+            contentPadding = PaddingValues(start = 10.dp)
+        ) {
+            Icon(Icons.Default.Report, contentDescription = "Feedback Icon")
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = stringResource(id = R.string.feedback))
+        }
 
-                Spacer(modifier = Modifier.height(10.dp))
-            }
-
-            item {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(10.dp)
-                ) {
-                    Button(
-                        onClick = { /* TODO: Feedback action */ },
-                        modifier = Modifier.weight(1f),
-                        contentPadding = PaddingValues(start = 10.dp)
-                    ) {
-                        Icon(Icons.Default.Report, contentDescription = "Feedback Icon")
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Text(text = stringResource(id = R.string.feedback))
-                    }
-
-                    Button(
-                        onClick = { /* TODO: Help action */ },
-                        modifier = Modifier.weight(1f),
-                        contentPadding = PaddingValues(start = 10.dp)
-                    ) {
-                        Icon(Icons.Default.Help, contentDescription = "Help Icon")
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Text(text = stringResource(id = R.string.help))
-                    }
-                }
-
-                Spacer(modifier = Modifier.height(10.dp))
-            }
-
-            item {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 10.dp),
-                    colors = CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.surfaceVariant
-                    ),
-                    elevation = CardDefaults.cardElevation(defaultElevation = 1.dp)
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(10.dp)
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.logs),
-                            fontSize = 15.sp,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Text(
-                            text = stringResource(id = R.string.logs_title_150),
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Text(
-                            text = stringResource(id = R.string.logs_text_150),
-                            fontSize = 13.sp,
-                            modifier = Modifier
-                                .padding(start = 10.dp)
-                                .fillMaxWidth()
-                        )
-                    }
-                }
-
-                Spacer(modifier = Modifier.height(20.dp))  // 添加更多的空白以确保可以滚动
-            }
+        Button(
+            onClick = { /* TODO: Help action */ },
+            modifier = Modifier.weight(1f),
+            contentPadding = PaddingValues(start = 10.dp)
+        ) {
+            Icon(Icons.Default.Help, contentDescription = "Help Icon")
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = stringResource(id = R.string.help))
         }
     }
 }
 
-
-
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar() {
-    TopAppBar(title = {
-        Text(text = stringResource(R.string.home))
-    })
+fun CustomTopBar(
+    title: String
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.padding(start = 8.dp) // 标题左侧间距
+            )
+        },
+        windowInsets = WindowInsets.safeDrawing.only(WindowInsetsSides.Top + WindowInsetsSides.Horizontal)
+    )
 }
