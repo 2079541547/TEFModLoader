ef390961ffd00860160801320752627f14df6d39
EternalFuture <142726636+2079541547@users.noreply.github.com>
2024-11-17 21:41:41 +0800

    更新文件

--- load/src/main/cpp/EFModLoader/include/EFModLoader/android/api.hpp
+++ load/src/main/cpp/EFModLoader/include/EFModLoader/android/api.hpp
@@ -1,18 +1,7 @@
-package eternal.future.efmodloader.load;
-
-import android.content.Context;
-import android.widget.Toast;
-
 /*******************************************************************************
-<<<<<<<< HEAD:load/src/main/cpp/EFModLoader/include/EFModLoader/android/api.hpp
  * 文件名称: api
  * 项目名称: TEFModLoader
  * 创建时间: 2024/11/9 上午4:07
-========
- * 文件名称: Toast
- * 项目名称: TEFModLoader
- * 创建时间: 2024/11/9 上午4:19
->>>>>>>> main:load/src/main/java/eternal/future/efmodloader/load/ToastApi.java
  * 作者: EternalFuture゙
  * Github: https://github.com/2079541547
  * 版权声明: Copyright © 2024 EternalFuture゙. All rights reserved.
@@ -32,46 +21,16 @@ import android.widget.Toast;
  * 注意事项: 请严格遵守GNU AGPL v3.0协议使用本代码，任何未经授权的商业用途均属侵权行为。
  *******************************************************************************/
 
+#pragma once
 
-<<<<<<<< HEAD:load/src/main/cpp/EFModLoader/include/EFModLoader/android/api.hpp
 #include <iostream>
 #include <jni.h>
-<<<<<<<< HEAD:load/src/main/cpp/EFModLoader/include/EFModLoader/android/api.hpp
 #include "../api/RegisterApi.hpp"
-========
-#include <EFModLoader/android/api.hpp>
->>>>>>>> main:load/src/main/cpp/EFModLoader/include/EFModLoader/android/Android.hpp
 
 namespace EFModLoader::Android::API {
 
     using namespace std;
-<<<<<<<< HEAD:load/src/main/cpp/EFModLoader/include/EFModLoader/android/api.hpp
     
     void Register();
 
-========
-
-    extern string* get_PackageName;
-    extern string* get_cacheDir;
-
-    void Load(JNIEnv *env, const string& EFModLoader);
-    void Load_Loader(JNIEnv *env, const string& EFModLoader);
->>>>>>>> main:load/src/main/cpp/EFModLoader/include/EFModLoader/android/Android.hpp
-========
-/**
- * 自定义Toast工具类，用于在Android应用中显示高度自定义的Toast消息。
- * 提供了多种方法来设置Toast的持续时间、位置、背景颜色、文本颜色等。
- */
-public class ToastApi {
-
-    /**
-     * 显示一个默认的Toast消息。
-     *
-     * @param context 应用上下文。
-     * @param message 要显示的消息。
-     */
-    public static void show(Context context, String message) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
-    }
->>>>>>>> main:load/src/main/java/eternal/future/efmodloader/load/ToastApi.java
 }
--- load/src/main/java/eternal/future/efmodloader/load/Loader.java
+++ load/src/main/java/eternal/future/efmodloader/load/Loader.java
@@ -3,7 +3,6 @@ package eternal.future.efmodloader.load;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.util.Log;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
