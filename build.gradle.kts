ee2ff96d3684eb11620e8c5be77446272f5dd332
EternalFuture゙ <142726636+2079541547@users.noreply.github.com>
2025-01-01 22:30:33 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,7 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-        alias(libs.plugins.android.library)
-}
-
-android {
-        namespace = "silkways.tefmodloader.core"
-        compileSdk = 35
-
-        defaultConfig {
-                minSdk = 24
-
-                testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-                consumerProguardFiles("consumer-rules.pro")
-                externalNativeBuild {
-                        cmake {
-                                cppFlags("-std=c++23")
-                        }
-                }
-        }
-
-        buildTypes {
-                release {
-                        isMinifyEnabled = false
-                        proguardFiles(
-                                getDefaultProguardFile("proguard-android-optimize.txt"),
-                                "proguard-rules.pro"
-                        )
-                }
-        }
-        externalNativeBuild {
-                cmake {
-                        path("src/main/cpp/CMakeLists.txt")
-                        version = "3.22.1"
-                }
-        }
-        compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-        }
-        ndkVersion = "28.0.12674087 rc2"
-}
-
-dependencies {
-
-        implementation(libs.androidx.appcompat)
-        implementation(libs.material)
-        testImplementation(libs.junit)
-        androidTestImplementation(libs.androidx.junit)
-        androidTestImplementation(libs.androidx.espresso.core)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.android.library) apply false
 }
