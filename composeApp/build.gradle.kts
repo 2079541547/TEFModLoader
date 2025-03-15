5f5d6f580c5e9fc1f21296466c0c29efc01590c9
2079541547 <2079541547@qq.com>
2025-03-15 17:35:06 +0800

    更新文件

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -70,7 +70,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1000
-        versionName = "10.0.0 Beta"
+        versionName = "10.0.0 Beta1"
     }
     packaging {
         resources {
